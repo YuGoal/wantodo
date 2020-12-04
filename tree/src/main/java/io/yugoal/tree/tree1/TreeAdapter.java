77c02c6bdfdcf48db364f0d545073a2e3494d363
caoyu <1127caoyu@gmail.com>
2020-12-04 16:15:18 +0800

    用FlexboxLayout替换适配器嵌套

--- tree/src/main/java/io/yugoal/tree/tree1/TreeAdapter.java
+++ tree/src/main/java/io/yugoal/tree/tree1/TreeAdapter.java
@@ -1,23 +1,29 @@
 package io.yugoal.tree.tree1;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.ObservableList;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.flexbox.FlexDirection;
-import com.google.android.flexbox.FlexWrap;
-import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.FlexboxLayout;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 import io.yugoal.lib_base.base.customview.BaseCustomViewModel;
 import io.yugoal.lib_base.base.recyclerview.BaseViewHolder;
 import io.yugoal.lib_common_ui.arouter.IAppService;
 import io.yugoal.lib_common_ui.arouter.RouteServiceManager;
+import io.yugoal.tree.R;
+import io.yugoal.tree.treelist.TreeListActivity;
+import io.yugoal.tree.views.TreeItem2Model;
 import io.yugoal.tree.views.TreeItemModel;
 import io.yugoal.tree.views.TreeItemView;
-import io.yugoal.tree.tree2.Tree2Adapter;
 
 
 /**
@@ -29,7 +35,7 @@ public class TreeAdapter extends RecyclerView.Adapter<BaseViewHolder> {
     private static final String TAG = "TreeAdapter";
     private ObservableList<BaseCustomViewModel> mItems;
     private Context context;
-    private RecyclerView recyclerView;
+    private Queue<TextView> mFlexItemTextViewCaches = new LinkedList<>();
 
     public TreeAdapter() {
         IAppService iAppService = RouteServiceManager.provide(IAppService.class, IAppService.APP_SERVICE);
@@ -44,7 +50,6 @@ public class TreeAdapter extends RecyclerView.Adapter<BaseViewHolder> {
     @Override
     public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         TreeItemView treeItemView = new TreeItemView(parent.getContext());
-        recyclerView = treeItemView.getViewDataBinding().recyclerView;
         return new BaseViewHolder(treeItemView);
     }
 
@@ -52,12 +57,20 @@ public class TreeAdapter extends RecyclerView.Adapter<BaseViewHolder> {
     public void onBindViewHolder(@NonNull BaseViewHolder holder, int position) {
 
         TreeItemModel treeBean = (TreeItemModel) mItems.get(position);
-
-        Tree2Adapter tree2Adapter = new Tree2Adapter();
-        tree2Adapter.setData(treeBean.item2Model);
-        FlexboxLayoutManager manager = new FlexboxLayoutManager(context, FlexDirection.ROW, FlexWrap.WRAP);
-        recyclerView.setLayoutManager(manager);
-        recyclerView.setAdapter(tree2Adapter);
+        TreeItemView treeItemView = (TreeItemView) holder.getView();
+        FlexboxLayout fbl = treeItemView.getViewDataBinding().fbl;
+        for (int i = 0; i < treeBean.item2Model.size(); i++) {
+            TreeItem2Model childItem = treeBean.item2Model.get(i);
+            TextView child = createOrGetCacheFlexItemTextView(fbl);
+            child.setText(childItem.name);
+            child.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TreeListActivity.show(v.getContext(),childItem.id,childItem.name);
+                }
+            });
+            fbl.addView(child);
+        }
         holder.bind(treeBean);
     }
 
@@ -67,5 +80,27 @@ public class TreeAdapter extends RecyclerView.Adapter<BaseViewHolder> {
             return mItems.size();
         }
         return 0;
+    }
+
+    @Override
+    public void onViewRecycled(@NonNull BaseViewHolder holder) {
+        super.onViewRecycled(holder);
+        TreeItemView treeItemView = (TreeItemView) holder.getView();
+        for (int i = 0; i < treeItemView.getViewDataBinding().fbl.getChildCount(); i++) {
+            mFlexItemTextViewCaches.offer((TextView) treeItemView.getViewDataBinding().fbl.getChildAt(i));
+        }
+        treeItemView.getViewDataBinding().fbl.removeAllViews();
+    }
+
+    private TextView createOrGetCacheFlexItemTextView(FlexboxLayout fbl) {
+        TextView tv = mFlexItemTextViewCaches.poll();
+        if (tv != null) {
+            return tv;
+        }
+        return createFlexItemTextView(fbl);
+    }
+
+    private TextView createFlexItemTextView(FlexboxLayout fbl) {
+        return (TextView) LayoutInflater.from(fbl.getContext()).inflate(R.layout.item_tree2, fbl, false);
     }
 }
